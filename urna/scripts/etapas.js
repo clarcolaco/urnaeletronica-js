c6a0c79e1102f31847ae1879337d879b2b237296
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-08-30 19:43:46 -0300

    Adicionando javascript

---
+++ urna/scripts/etapas.js
@@ -0,0 +1,95 @@
+let etapas = [
+
+    {
+
+        titulo: 'GOVERNADORES',
+        numeros: 5,
+        candidatos: [
+
+            {
+                numero: 54345,
+                name: 'Coragem',
+                partido: 'Dos medrosos',
+                fotos: [
+                    { url: 'candidato1.jpg', legenda: 'Presidente' },
+                ]
+
+            },
+            {
+                numero: 34554,
+                name: 'Coragem',
+                partido: 'Dos medrosos',
+                fotos: [
+                    { url: 'candidato2.jpg', legenda: 'Presidente' },
+                ]
+
+            },
+            {
+                numero: 23054,
+                name: 'Coragem',
+                partido: 'Dos medrosos',
+                fotos: [
+                    { url: 'candidato3.jpg', legenda: 'Presidente' },
+                ]
+
+            },
+
+        ]
+    },
+
+
+
+    {
+
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+
+            {
+                numero: 54,
+                name: 'Coragem',
+                partido: 'Dos medrosos',
+                vice: 'Pateta',
+                fotos: [
+                    { url: 'candidato1.jpg', legenda: 'Presidente' },
+                    { url: 'vice1.png', legenda: 'Vice-Presidente' },
+
+                ]
+
+            },
+
+            {
+                numero: 10,
+                name: 'Steven',
+                partido: 'Dos fofos',
+                vice: 'Pipu',
+                fotos: [
+                    { url: 'candidato2.jpg', legenda: 'Presidente' },
+                    { url: 'vice2.png', legenda: 'Vice-Presidente' },
+
+                ]
+
+            },
+
+            {
+                numero: 54,
+                name: 'Lula molusco',
+                partido: 'Dos bravos',
+                vice: 'Batman',
+                fotos: [
+                    { url: 'candidato3.jpg', legenda: 'Presidente' },
+                    { url: 'vice3.png', legenda: 'Vice-Presidente' },
+
+                ]
+
+            }
+        ]
+
+
+
+    }
+
+
+
+
+];
---
+++ urna/scripts/script.js
@@ -0,0 +1,28 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+
+
+//variaveis de ambiente
+
+
+
+function clicou(n) {
+    alert("Clicou em " + n);
+}
+
+function branco() {
+    alert("Clicou em branco");
+}
+
+function corrige() {
+    alert("Clicou em corrige");
+}
+
+function confirma() {
+    alert("Clicou em  confirma");
+}
